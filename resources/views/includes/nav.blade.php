87f827175260b870e22574c0cacd16e66e457e3b
ari_fakhri <arifakhri97@gmail.com>
2016-09-11 20:17:19 +0700

    add code for make navbar

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -1,23 +1,20 @@
+<ul id="dropdown" class="dropdown-content">
+    <li><a href="#!">one<span class="badges"></span></a></li>
+    <li><a href="#!">two<span class="new badge"></span></a></li>
+    <li class="divider"></li>
+    <li class="divider"></li>
+    <li><a href="#">Outbox<span class="badge">14</span></a></li>
+  </ul>
 
+<nav>
 
-
-    <nav>
     <div class="nav-wrapper">      
       <a href="#" class="brand-logo">Logo</a>
-      <ul id="nav-mobile" class="right hide-on-med-and-down">
-      <ul class="dropdown-content">
-    <li><a href="#!">one</a></li>
-    <li><a href="#!">two</a></li>
-    <li class="divider"></li>
-    <li><a href="#!">three</a></li>
-  </ul>
-        <li><a href="sass.html">Sass</a></li>
-        <li><a href="badges.html">Components</a></li>
-        <li><a href="collapsible.html">JavaScript</a></li>
-        <li><a href="mobile.html"><i class="material-icons">more_vert</i></a></li>
-        <li><a class="waves-effect waves-light btn-large" onclick="">A large button</a></li>
+      <ul class="right hide-on-med-and-down"> 
+        <li><a href="sass.html">Home</a></li>
+        <li><a class="dropdown-button" href="/" data-activates="dropdown">Menu<i class="material-icons right">arrow_drop_down</i></a></li>
       </ul>
 
-      
     </div>
   </nav>
+        
