1da0abbb643aaf49c283132b15241362e6fe785e
ari_fakhri <arifakhri97@gmail.com>
2016-09-12 17:24:15 +0700

    deleted <script>

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -20,14 +20,7 @@
     <script type="text/javascript" src="js/app.js"></script>
     <script type="text/javascript" src="js/materialize.js"></script>
     <script>
-    $(document).ready(function(){
-    $('.carousel.carousel-slider').carousel({
-        full_width: true
-    });
 
-    $(".dropdown-button").dropdown();
-        
-        });
     </script>
     <!-- Navbar -->
      @include('includes.nav')
