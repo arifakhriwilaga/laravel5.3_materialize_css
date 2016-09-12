551e854c8fd4e3e6cb75f43c798de956ec96669c
ari_fakhri <arifakhri97@gmail.com>
2016-09-12 16:55:45 +0700

    make app.blade.php for page templated blade

---
+++ resources/app.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+              <!--Import jQuery before materialize.js-->
+        <title>Laravel</title>
+          <!--Link Material Design Icon -->
+          <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+          <!--Import materialize.css-->
+          <link type="text/css" rel="stylesheet" href="css/materialize.css"  media="screen,projection"/>
+        <style>
+            .row .col.s3 {
+              width: 20%;
+              margin-left: auto;
+              left: auto;
+              right: auto;
+            }
+        </style>
+    </head>
+    <body>
+    <script type="text/javascript" src="js/jquery-3.1.0.js"></script>
+    <script type="text/javascript" src="js/app.js"></script>
+    <script type="text/javascript" src="js/materialize.js"></script>
+    <!-- Custom JS for popup photos -->>
+    <script>
+    $(document).ready(function(){
+    $('.materialboxed').materialbox();
+        });
+    </script>
+
+    <!-- Navbar -->
+     @include('includes.nav')
+
+     <!-- Content -->
+     <div class="row">
+          @yield('content')
+    </div>
+    </body>
+</html>
