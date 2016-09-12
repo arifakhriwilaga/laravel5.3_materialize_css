3692159c8702c1f498fbeb78b4d1eaf7a3b3502a
ari_fakhri <arifakhri97@gmail.com>
2016-09-12 16:54:12 +0700

    add card with image raja-ampat and neated code

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,130 +1,79 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('app')
 
-              <!--Import jQuery before materialize.js-->
-   
-
-        <title>Laravel</title>
-        <!--Link Material Design Icon -->
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
-       <!--Import materialize.css-->
-      <link type="text/css" rel="stylesheet" href="css/materialize.css"  media="screen,projection"/>
-
-       <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway';
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-
-            .carousel-item {
-                height: 500px;
-                width: 1000px;
-                margin: 0 auto;
-            }
-            
-        </style>
-    </head>
-    <body>
-    <script type="text/javascript" src="js/jquery-3.1.0.js"></script>
-    <script type="text/javascript" src="js/app.js"></script>
-    <script type="text/javascript" src="js/materialize.js"></script>
-    <script>
-    $(document).ready(function(){
-    $('.carousel.carousel-slider').carousel({
-        full_width: true
-    });
-
-    $(".dropdown-button").dropdown();
-        
-        });
-    </script>
-
-     @include('includes.nav')
-
-            <div class="content">
-
-            <div class="carousel carousel-slider center" data-indicators="true">
-                <div class="carousel-fixed-item center">
-      <a class="btn waves-effect white grey-text darken-text-2">button</a>
+@section('content')
+    <div class="col s3">
+        <div class="card-panel teal">
+          <span class="white-text"></span>
+        </div>
     </div>
-            <div class="carousel-item red white-text" href="#one!">
-               <p class="white-text">Here!</p>
+    <div class="col s3">
+        <div class="card-panel teal">
+            <span class="white-text"></span>
+        </div>
+    </div>
+    <div class="col s3">
+        <div class="card-panel teal">
+          <span class="white-text"></span>
+        </div>
+    </div>
+    <div class="col s3">
+        <div class="card">
+            <div class="card-image">
+              <img class="materialboxed" src="img/home/gunung-bromo.jpg">
             </div>
-            <div class="carousel-item amber white-text" href="#one!">
-                <p class="white-text"><img width=1200px src="img/2.jpeg" ></p>
-            </div>
-            <div class="carousel-item green white-text" href="#one!">    
-                <p class="white-text"><img width=1200px src="img/3.jpg"></p>
-            </div>
-                
-              </div>
-
-                <div class="title m-b-md">
-                    Laravel
-
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+            <div class="card-content">
+            <span>
+              <p>Don't forget view Bromo Mountain Indonesia</p>
+            </span>
             </div>
         </div>
-
-    </body>
-</html>
+    </div>
+    <div class="col s3">
+        <div class="card">
+            <div class="card-image">
+              <img class="materialboxed" width="650" src="img/home/gunung-jaya-wijaya.jpg">
+            </div>
+            <div class="card-content">
+            <span>
+              <p>Bigest Mount in Indonesia, Jaya Wijaya. very beautiful</p>
+            </span>
+            </div>
+        </div>
+    </div>
+    <div class="col s3">
+        <div class="card">
+            <div class="card-image">
+              <img class="materialboxed" src="img/home/gunung-rinjani.jpg">
+            </div>
+            <div class="card-content">
+            <span>
+              <p>Rinjani Mountain.</p>
+            </span>
+            </div>
+        </div>
+    </div>
+    <div class="col s3">
+        <div class="card">
+            <div class="card-image">
+              <img class="materialboxed" src="img/home/pulau-lengkuas-belitung.jpg">
+            </div>
+            <div class="card-content">
+            <span>
+              <p>Lengkuas Island in Belitung Indonesia</p>
+            </span>
+            </div>
+        </div>
+    </div>
+    <div class="col s3">
+        <div class="card">
+            <div class="card-image">
+              <img class="materialboxed" src="img/home/raja-ampat.jpg">
+            </div>
+            <div class="card-content">
+            <span>
+              <p>Raja Ampat Island, soo very...</p>
+            </span>
+            </div>
+        </div>
+    </div>
+@endsection
