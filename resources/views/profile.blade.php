187f52767680ef6baada4dfbb2c878e4985d56e3
ari_fakhri <arifakhri97@gmail.com>
2016-09-13 17:33:23 +0700

    design layout profile.blade.php

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,39 @@
+@extends('app')
+@section('content')
+<div class="container">
+
+	<div class="row">
+		<div class="col s12">
+			&nbsp;
+		</div>
+   		<div class="col s4">
+	   		<h3>Ari Fakhri</h3>
+	   		<img src="img/profile.gif" class="circle responsive-img" width="200px" height="200px" alt="Responsive image">
+	   </div>
+	   	<div class="col s8">
+	   		<table class="highlight">
+ 				<tr>
+ 					<td width=""><b>Nama</b></td>
+ 					<td>:</td>
+ 					<td>Ari Fakhri Wilaga</td>
+ 				</tr>
+ 				<tr>
+ 					<td><b>Tempat, Tanggal Lahir</b></td>
+ 					<td>:</td>
+ 					<td>Tasikmalaya, 02 Juni 1997</td>
+ 				</tr>
+ 				<tr>
+ 					<td><b>Jenis Kelamin</b></td>
+ 					<td>:</td>
+ 					<td>Laki-laki</td>
+ 				</tr>
+ 				<tr>
+ 					<td><b>Alamat Domisili</b></td>
+ 					<td>:</td>
+ 					<td>Alam Sanggar Indah Pinus Citapen-Bandung Barat</td>
+ 				</tr>
+			</table>
+	   	</div>
+	</div>
+</div>
+@endsection
