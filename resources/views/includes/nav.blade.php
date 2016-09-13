30e806f02a094880b75fe49b7be7b9f9436969e5
ari_fakhri <arifakhri97@gmail.com>
2016-09-13 17:30:47 +0700

    add icon menu profile and contact

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -5,6 +5,8 @@
       </ul>
       <ul class="right hide-on-med-and-down"> 
         <li><a href="/"><i class="material-icons">dashboard</i></a></li>
+        <li><a href="profile"><i class="material-icons">perm_identity</i></a></li>
+        <li><a href="contact"><i class="material-icons">call</i></a></li>
         <li> &nbsp;</li>
       </ul>
     </div>
